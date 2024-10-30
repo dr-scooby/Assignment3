0dc4931969ca0087830d096c69d1a10a0e6908d5
dr-scooby <hinurali@hotmail.com>
2024-10-29 21:06:13 -0700

    added User class

--- src/main/java/com/jah/jismail3/Controller.java
+++ src/main/java/com/jah/jismail3/Controller.java
@@ -2,6 +2,8 @@ package com.jah.jismail3;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,6 +23,18 @@ public class Controller {
     @GetMapping("/foo")
     public String getReply(){
         return "bar";
+    }
+
+
+    // end point url: localhost:8080/hello
+    @PostMapping("/hello")
+    public String getHello(@RequestBody User user){
+        // get the data from the client body - JSON, and convert to User class
+        User auser = user;
+        System.out.println("\n\nGot user:: " + auser.getName());
+        // return the user name
+        return "hello " + auser.getName() + "!\n";
+
     }
 
 }
---
+++ src/main/java/com/jah/jismail3/User.java
@@ -0,0 +1,21 @@
+package com.jah.jismail3;
+
+public class User {
+
+    private String name;
+
+
+    public User() {
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
